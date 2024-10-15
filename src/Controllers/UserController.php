e49775563d114bff6b86e211c8ce31dab39a484b
rafaels549 <rafalesantoscostat@gmail.com>
2024-10-15 12:46:06 -0300

    Change the reponse to English

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -1,9 +1,11 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Rafael\SubwayRoutesApi\Database\Entity\User;
 use Rafael\SubwayRoutesApi\Database\Database;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 use Ramsey\Uuid\Uuid;
 
 return function (App $app) {
@@ -12,32 +14,23 @@ return function (App $app) {
 
     $app->group('/user', function (RouteCollectorProxy $group) use ($entityManager) {
 
-        $group->post('/create', function (Request $request, Response $response, $args) use ($entityManager) {
+        $group->post('/create', function (Request $request, Response $response) use ($entityManager) {
             $body = $request->getBody()->getContents();
             $data = json_decode($body, true);
-    
+
             if (json_last_error() !== JSON_ERROR_NONE) {
-                $response->getBody()->write(json_encode(['error' => 'JSON inválido']));
+                $response->getBody()->write(json_encode(['error' => 'Invalid JSON format']));
                 return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
             }
-    
-            if (
-                !isset(
-                    $data['username'],
-                    $data['password'],
-                    $data['role_id'],
-                    $data['contact'],
-                    $data['address'],
-                    $data['date_of_birth'],
-                    $data['gender'],
-                    $data['nationality'],
-                    $data['languages']
-                )
-            ) {
-                $response->getBody()->write(json_encode(['error' => 'Dados inválidos']));
-                return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+
+            $requiredFields = ['username', 'password', 'role_id', 'contact', 'address', 'date_of_birth', 'gender', 'nationality', 'languages'];
+            foreach ($requiredFields as $field) {
+                if (!isset($data[$field])) {
+                    $response->getBody()->write(json_encode(['error' => "Missing required field: $field"]));
+                    return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+                }
             }
-    
+
             $roleId = Uuid::fromString($data['role_id']);
             $user = new User(
                 $data['username'],
@@ -55,18 +48,13 @@ return function (App $app) {
                 $data['nationality'],
                 $data['languages']
             );
-    
+
             $entityManager->persist($user);
             $entityManager->flush();
-    
-            $response->getBody()->write(json_encode([
-                'message' => 'Usuário criado com sucesso'
-            ]));
-    
+
+            $response->getBody()->write(json_encode(['message' => 'User created successfully']));
             return $response->withStatus(201)->withHeader('Content-Type', 'application/json');
         });
-    
+
     });
-    
-  
 };
