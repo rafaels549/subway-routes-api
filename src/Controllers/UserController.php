6bb3758610216be4de5c88f7a1afe95408c3b256
rafaels549 <rafalesantoscostat@gmail.com>
2024-10-31 17:20:52 -0300

    Changing the reponse adding data and message, total of numbers of users returned, adding asc and desc, and adding more parameter for search

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -42,25 +42,135 @@ return function (App $app) {
                 return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
             }
         })->add(new UserValidationMiddleware());
-        $group->get('/all', function (Request $request, Response $response) use ($entityManager) {
+        $group->get('', function (Request $request, Response $response) use ($entityManager) {
             $queryParams = $request->getQueryParams();
             $skip = isset($queryParams['skip']) ? (int) $queryParams['skip'] : 0;
             $limit = isset($queryParams['limit']) ? (int) $queryParams['limit'] : 10;
-            $name = isset($queryParams['name']) ? $queryParams['name'] : '';
-            $qb = $entityManager->getRepository(User::class)->createQueryBuilder('u')
-                ->setFirstResult($skip)
-                ->setMaxResults($limit);
+            $sort = $queryParams['sort'] ?? 'username';
+            $order = $queryParams['order'] ?? 'asc';
         
-            if (!empty($name)) {
-                $qb->where($qb->expr()->like('u.username', ':name'))
-                   ->setParameter('name', '%' . $name . '%');
+            $qb = $entityManager->getRepository(User::class)->createQueryBuilder('u');
+        
+            if (!empty($queryParams['username'])) {
+                $qb->andWhere($qb->expr()->like('u.username', ':username'))
+                   ->setParameter('username', '%' . $queryParams['username'] . '%');
             }
+            if (!empty($queryParams['email'])) {
+                $qb->andWhere($qb->expr()->like('u.email', ':email'))
+                   ->setParameter('email', '%' . $queryParams['email'] . '%');
+            }
+            if (!empty($queryParams['phone'])) {
+                $qb->andWhere($qb->expr()->like('u.phone', ':phone'))
+                   ->setParameter('phone', '%' . $queryParams['phone'] . '%');
+            }
+            if (!empty($queryParams['date_of_birth'])) {
+                $qb->andWhere('u.date_of_birth = :date_of_birth')
+                   ->setParameter('date_of_birth', $queryParams['date_of_birth']);
+            }
+            if (!empty($queryParams['gender'])) {
+                $qb->andWhere('u.gender = :gender')
+                   ->setParameter('gender', $queryParams['gender']);
+            }
+            if (!empty($queryParams['nationality'])) {
+                $qb->andWhere('u.nationality = :nationality')
+                   ->setParameter('nationality', $queryParams['nationality']);
+            }
+            if (!empty($queryParams['languages'])) {
+                $qb->andWhere($qb->expr()->like('u.languages', ':languages'))
+                   ->setParameter('languages', '%' . $queryParams['languages'] . '%');
+            }
+            if (!empty($queryParams['street'])) {
+                $qb->andWhere($qb->expr()->like('u.street', ':street'))
+                   ->setParameter('street', '%' . $queryParams['street'] . '%');
+            }
+            if (!empty($queryParams['city'])) {
+                $qb->andWhere($qb->expr()->like('u.city', ':city'))
+                   ->setParameter('city', '%' . $queryParams['city'] . '%');
+            }
+            if (!empty($queryParams['state'])) {
+                $qb->andWhere($qb->expr()->like('u.state', ':state'))
+                   ->setParameter('state', '%' . $queryParams['state'] . '%');
+            }
+            if (!empty($queryParams['country'])) {
+                $qb->andWhere($qb->expr()->like('u.country', ':country'))
+                   ->setParameter('country', '%' . $queryParams['country'] . '%');
+            }
+            if (!empty($queryParams['postal_code'])) {
+                $qb->andWhere('u.postal_code = :postal_code')
+                   ->setParameter('postal_code', $queryParams['postal_code']);
+            }
+        
+            if (in_array($sort, ['username', 'date_of_birth', 'created_at'])) {
+                $qb->orderBy('u.' . $sort, $order === 'desc' ? 'DESC' : 'ASC');
+            }
+            $qb->setFirstResult($skip)
+               ->setMaxResults($limit);
         
             $users = $qb->getQuery()->getResult();
             $userData = array_map(fn($user) => (new UserDTO($user))->toArray(), $users);
         
-            $response->getBody()->write(json_encode($userData));
-            return $response->withHeader('Content-Type', 'application/json');
+            $countQueryBuilder = $entityManager->getRepository(User::class)->createQueryBuilder('u')
+                ->select('COUNT(u.id)');
+        
+            if (!empty($queryParams['username'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.username', ':username'))
+                                  ->setParameter('username', '%' . $queryParams['username'] . '%');
+            }
+            if (!empty($queryParams['email'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.email', ':email'))
+                                  ->setParameter('email', '%' . $queryParams['email'] . '%');
+            }
+            if (!empty($queryParams['phone'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.phone', ':phone'))
+                                  ->setParameter('phone', '%' . $queryParams['phone'] . '%');
+            }
+            if (!empty($queryParams['date_of_birth'])) {
+                $countQueryBuilder->andWhere('u.date_of_birth = :date_of_birth')
+                                  ->setParameter('date_of_birth', $queryParams['date_of_birth']);
+            }
+            if (!empty($queryParams['gender'])) {
+                $countQueryBuilder->andWhere('u.gender = :gender')
+                                  ->setParameter('gender', $queryParams['gender']);
+            }
+            if (!empty($queryParams['nationality'])) {
+                $countQueryBuilder->andWhere('u.nationality = :nationality')
+                                  ->setParameter('nationality', $queryParams['nationality']);
+            }
+            if (!empty($queryParams['languages'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.languages', ':languages'))
+                                  ->setParameter('languages', '%' . $queryParams['languages'] . '%');
+            }
+            if (!empty($queryParams['street'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.street', ':street'))
+                                  ->setParameter('street', '%' . $queryParams['street'] . '%');
+            }
+            if (!empty($queryParams['city'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.city', ':city'))
+                                  ->setParameter('city', '%' . $queryParams['city'] . '%');
+            }
+            if (!empty($queryParams['state'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.state', ':state'))
+                                  ->setParameter('state', '%' . $queryParams['state'] . '%');
+            }
+            if (!empty($queryParams['country'])) {
+                $countQueryBuilder->andWhere($countQueryBuilder->expr()->like('u.country', ':country'))
+                                  ->setParameter('country', '%' . $queryParams['country'] . '%');
+            }
+            if (!empty($queryParams['postal_code'])) {
+                $countQueryBuilder->andWhere('u.postal_code = :postal_code')
+                                  ->setParameter('postal_code', $queryParams['postal_code']);
+            }
+        
+            $totalUsers = $countQueryBuilder->getQuery()->getSingleScalarResult();
+        
+            $responseData = [
+                'message' => empty($userData) ? 'No users found.' : 'Users found.',
+                'data' => $userData,
+                'totalNumber' => $totalUsers
+            ];
+        
+            $response->getBody()->write(json_encode($responseData));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
         });
     });
 };
